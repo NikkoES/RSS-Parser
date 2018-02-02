9f59bd2563362b5b7f835716d14845b2ae8184b4
NikkoES <nikkoeka04@gmail.com>
2018-02-02 17:40:58 +0700

    Initial commit

--- app/src/main/java/com/prof/rssparser/example/MainActivity.java
+++ app/src/main/java/com/prof/rssparser/example/MainActivity.java
@@ -53,8 +53,8 @@ public class MainActivity extends AppCompatActivity {
     private ArticleAdapter mAdapter;
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private ProgressBar progressBar;
-    //private String urlString = "https://www.androidauthority.com/feed";
-    private String urlString = "https://www.skynewsarabia.com/web/rss/markets.xml";
+    private String urlString = "http://if.uinsgd.ac.id/feed";
+//    private String urlString = "https://www.skynewsarabia.com/web/rss/markets.xml";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
